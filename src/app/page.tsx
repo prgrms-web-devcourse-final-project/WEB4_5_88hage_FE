3165f4b9c8419fba649a85fd5c9dc606a8ab1ec8
WON <won431236@gmail.com>
2025-07-08 15:11:17 +0900

    행사 디테일 페이지 (진행중)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import GroupDetail from '@/pages/GroupDetail';
+import EventDetail from '@/pages/EventDetail';
 
 export default function page() {
   return (
     <>
-      <GroupDetail />
+      {/* <GroupDetail /> */}
+      <EventDetail />
     </>
   );
 }
