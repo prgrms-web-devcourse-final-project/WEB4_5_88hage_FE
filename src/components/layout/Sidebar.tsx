c9dbad146c0a6246eddb292957799411bb22ca00
dlakrp123 <dlakrp731@gmail.com>
2025-07-12 16:36:02 +0900

    Refactor: Simplify Header component and remove unused props; add Sidebar component

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+  return (
+    <>
+      <div className="w-[270px]"></div>
+    </>
+  );
+}
--- src/pages/FaqNotice.tsx
+++ src/pages/FaqNotice.tsx
@@ -1,18 +1,11 @@
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
-import FaqSection from "@/components/faq/Section";
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
+import FaqSection from '@/components/faq/Section';
 
 export default function FaqPage() {
   return (
     <>
-      <Header
-        headerBgClass="bg-[#1d1d1d]"
-      >
-      </Header>
-
       <FaqSection />
-
-      <Footer />
     </>
   );
 }
