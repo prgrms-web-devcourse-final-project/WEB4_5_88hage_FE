9c414100d4c66f697702b0ae406646b866f9c9fe
dlakrp123 <dlakrp731@gmail.com>
2025-07-14 00:55:46 +0900

    fix: adjust margin for greeting header in MyGathering component

--- src/pages/MyGathering.tsx
+++ src/pages/MyGathering.tsx
@@ -5,7 +5,7 @@ export default function MyGathering() {
   return (
     <>
       <div className="p-5">
-        <h1 className="h2 mb-5 text-white">안녕하세요, 홍길동님 🖐️</h1>
+        <h1 className="h2 mb-5 text-white lg:mb-10">안녕하세요, 홍길동님 🖐️</h1>
         <h2 className="h3 text-white">모임</h2>
         <div className="lg:flex lg:min-w-[1220px] lg:items-center lg:justify-center">
           {/* 고정 너비 사이드 */}
