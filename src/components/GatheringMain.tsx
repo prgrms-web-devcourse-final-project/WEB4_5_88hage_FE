caadffc346b82125ab38f4890d587a2caaf64c0c
dlakrp123 <dlakrp731@gmail.com>
2025-07-14 01:11:41 +0900

    Feat: mainPostHeader 컴포넌트 분리

--- src/components/GatheringMain.tsx
+++ src/components/GatheringMain.tsx
@@ -1,38 +1,14 @@
 import Image from 'next/image';
-import test from '@/assets/images/thinking.svg';
-import { EllipsisVertical, Users2 } from 'lucide-react';
 import Tag from './common/Tag';
 import mapIcon from '@/assets/images/map_icon.svg';
 import testMap from '@/assets/images/testmap.png';
+import MainPostHeader from './common/MainPostHeader';
 
 export default function GatheringMain() {
   return (
     <>
       <div className="bg-gray-7 lg:border-gray-5 mt-5 flex h-full w-full flex-col rounded-[15px] p-5 lg:border">
-        <div className="flex items-start lg:items-center">
-          <Image
-            src={test}
-            width={50}
-            height={50}
-            alt="profileImage"
-            className="rounded-full"
-          />
-          <div className="ml-2 lg:flex lg:items-center">
-            <h1 className="gradient-text h2 mb-2 text-[16px] lg:mb-0 lg:text-[24px]">
-              2025 그린 페스티벌
-            </h1>
-            <div className="bg-gray-4 t3 w-fit rounded-[30px] px-3 py-1 text-white lg:ml-5">
-              콘서트 🎤
-            </div>
-          </div>
-
-          <div className="text-gray-disabled ml-auto flex items-center gap-2">
-            <div className="t3 hidden lg:block">3명</div>
-            <Users2 className="h-[20px] w-[20px]" />
-            <EllipsisVertical className="h-[20px] w-[20px]" />
-          </div>
-        </div>
-        <hr className="text-gray-disabled mt-5" />
+        <MainPostHeader />
         <div className="mt-5 flex gap-5">
           <Tag name="힙한" />
           <Tag name="젊은" />
---
+++ src/components/common/MainPostHeader.tsx
@@ -0,0 +1,34 @@
+import { EllipsisVertical, Users2 } from 'lucide-react';
+import Image from 'next/image';
+import test from '@/assets/images/thinking.svg';
+
+export default function MainPostHeader() {
+  return (
+    <>
+      <div className="flex items-start lg:items-center">
+        <Image
+          src={test}
+          width={50}
+          height={50}
+          alt="profileImage"
+          className="rounded-full"
+        />
+        <div className="ml-2 lg:flex lg:items-center">
+          <h1 className="gradient-text h2 mb-2 text-[16px] lg:mb-0 lg:text-[24px]">
+            2025 그린 페스티벌
+          </h1>
+          <div className="bg-gray-4 t3 w-fit rounded-[30px] px-3 py-1 text-white lg:ml-5">
+            콘서트 🎤
+          </div>
+        </div>
+
+        <div className="text-gray-disabled ml-auto flex items-center gap-2">
+          <div className="t3 hidden lg:block">3명</div>
+          <Users2 className="h-[20px] w-[20px]" />
+          <EllipsisVertical className="h-[20px] w-[20px]" />
+        </div>
+      </div>
+      <hr className="text-gray-disabled mt-5" />
+    </>
+  );
+}
