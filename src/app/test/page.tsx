932999903e76df92395323e6c7937ee97d56010a
dlakrp123 <dlakrp731@gmail.com>
2025-07-14 15:15:54 +0900

    Test page 초기화

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,14 +1,3 @@
-import InquiryDetail from '@/pages/InquiryDetail';
-import InquiryListPage from '@/pages/InquiryList';
-import MyCalendar from '@/pages/MyCalendar';
-import MyGathering from '@/pages/MyGathering';
-import MyGatheringList from '@/pages/MyGatheringList';
-import Profile from '@/pages/Profile';
-
 export default function page() {
-  return (
-    <>
-      <Profile />
-    </>
-  );
+  return <></>;
 }
