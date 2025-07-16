093b087a744d703be83c9583243fa8d8046e288b
Ujin <tladbwls0430@gmail.com>
2025-07-16 13:59:35 +0900

    디코딩

--- src/pages/GatheringDetail.tsx
+++ src/pages/GatheringDetail.tsx
@@ -7,10 +7,19 @@ import testmap from '@/assets/images/testmap.png';
 import { LucideChevronDown, LucideHeart, LucideMapPin } from 'lucide-react';
 import GatheringHostBox from '@/components/GatheringHostBox';
 import { useAuthStore } from '@/stores/UseAuthStore';
+import { useEffect } from 'react';
 
 export default function GatheringDetail() {
-  const isAuthenticated = useAuthStore((s) => s.isAuthenticated)
-  console.log('로그인된 사용자 여부:', isAuthenticated)
+  const isAuthenticated = useAuthStore(s => s.isAuthenticated)
+  const user= useAuthStore(s => s.user)
+
+  useEffect(() => {
+    console.log('로그인 여부:', isAuthenticated)
+    if (user) {
+      console.log('이메일:',user.email)
+      console.log('닉네임:',user.nickname)
+    }
+  }, [isAuthenticated, user])
   return (
     <div className="eventDetail-gradient flex w-screen min-w-screen justify-center bg-[#121212] lg:w-340">
       <div className="hidden h-full min-h-screen py-15 text-[#f6f6f6] lg:flex">
