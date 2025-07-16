6f7b8daeeadac5326c0ac27385d04081829fe15f
Ujin <tladbwls0430@gmail.com>
2025-07-16 13:59:35 +0900

    디코딩

--- src/stores/UseAuthStore.ts
+++ src/stores/UseAuthStore.ts
@@ -1,14 +1,18 @@
+'use client';
+
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 import axios from 'axios'
+import base64 from 'base-64'
 
 interface User {
-  id: string
+  //id: string
   email: string
   nickname: string
 }
 
 interface AuthState {
+  token:string|null
   user: User | null
   isAuthenticated: boolean
   login: (email: string, pw: string, remember: boolean) => Promise<void>
@@ -20,24 +24,41 @@ const API = process.env.NEXT_PUBLIC_API_URL
 export const useAuthStore = create<AuthState>()(
   persist(
     (set) => ({
+      token:null,
+      user:null,
       isAuthenticated: false,
 
       login: async (email, pw, remember) => {
-        await axios.post(
-          `${API}auth/login`,
+        const {data}= await axios.post(
+          `${API}api/auth/login`,
           { email, password: pw, rememberMe: remember },
           { withCredentials: true }
         )
-        set({ isAuthenticated: true })
-      },
+        const token=data.data.accessToken as string
+        const [,payload]=token.split('.')
+        const decodePayload=JSON.parse(base64.decode(payload))
 
+        console.log('[AuthStore] decodePayload: ', decodePayload)
+
+        set({ 
+          token,
+          user:{
+            email:decodePayload.sub,
+            nickname: decodePayload.nickname
+          },
+          isAuthenticated: true })
+      },
       logout: () => {
-        set({ isAuthenticated: false, })
+        set({
+          token:null,
+          user:null,
+          isAuthenticated: false, })
       },
     }),
     {
       name: 'authState',
       partialize: (state) => ({
+        token:state.token,
         isAuthenticated: state.isAuthenticated,
         user: state.user,
       }),
