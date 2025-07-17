ba2d5c11493515ec1c9cbebad3dff73d103858c8
WON <won431236@gmail.com>
2025-07-18 00:28:46 +0900

    문의글 작성 (진행중)

--- src/components/ui/AddPhotoButton.tsx
+++ src/components/ui/AddPhotoButton.tsx
@@ -1,15 +1,67 @@
-import { FaCamera } from "react-icons/fa";
-import { twMerge } from "tailwind-merge";
+'use client';
+import { ChangeEvent, useState } from 'react';
+import { FaCamera } from 'react-icons/fa';
+import { twMerge } from 'tailwind-merge';
 
-export default function AddPhotoButton({className}:{className?:string}){
+export default function AddPhotoButton({
+  className,
+  onDataChange,
+}: {
+  className?: string;
+  onDataChange?: (data: FileList) => void;
+}) {
+  const [postImgList, setPostImgList] = useState<any>([]);
+
+  const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setPostImgList([]);
+      for (let i = 0; i < e.target.files.length; i++) {
+        const currentImgUrl = URL.createObjectURL(e.target.files[i]);
+        setPostImgList((prev: any) => [...prev, currentImgUrl].slice(0, 5));
+      }
+      if (onDataChange) onDataChange(e.target.files);
+    }
+  };
+
   return (
     <>
-    <div className={twMerge("lg:mt-[-20px] lg:mb-[-5px]", className)}>
-        <h3 className="text-main font-semibold mb-[13px] text-[16px] lg:text-[24px]">사진</h3>
-        <button className="w-[50px] h-[50px] rounded-[5px] border border-[#343434] flex justify-center items-center lg:w-[80px] lg:h-[80px]">
-            <FaCamera color="#343434" size={35} />;
-        </button>
-    </div>
+      <div className={twMerge('lg:mt-[-20px] lg:mb-[-5px]', className)}>
+        <h3 className="text-main mb-[13px] text-[16px] font-semibold lg:text-[24px]">
+          사진
+        </h3>
+        {postImgList && postImgList.length === 0 && (
+          <label
+            htmlFor="images"
+            className="flex size-[50px] cursor-pointer items-center justify-center gap-3 rounded-[5px] border border-[#343434] lg:h-[80px] lg:w-[80px]"
+          >
+            <FaCamera color="#343434" size={35} />
+          </label>
+        )}
+        {postImgList && postImgList.length > 0 && (
+          <label
+            htmlFor="images"
+            className="flex cursor-pointer items-center justify-start gap-3"
+          >
+            {postImgList.map((src: any, index: number) => (
+              <img
+                src={src}
+                alt=""
+                key={index}
+                className="max-h-30 rounded-[5px] border border-[#343434] p-2 lg:max-h-40"
+              />
+            ))}
+          </label>
+        )}
+        <input
+          type="file"
+          name="images"
+          id="images"
+          multiple
+          className="hidden"
+          accept="image/*"
+          onChange={handleImageUpload}
+        />
+      </div>
     </>
   );
-};
+}
--- src/pages/InquiryCreatePage.tsx
+++ src/pages/InquiryCreatePage.tsx
@@ -1,50 +1,93 @@
+'use client';
 import GrayButton from '@/components/button/GrayButton';
 import CategorySelect from '@/components/layout/CategorySelect';
 import WritingForm from '@/components/layout/WritingForm';
 import AddPhotoButton from '@/components/ui/AddPhotoButton';
+import { FormEvent } from 'react';
 
 export default function InquiryCreatePage() {
+  let formData = new FormData();
+  let imgData = new FormData();
+
+  const handleDataChange = (data: FileList) => {
+    imgData = new FormData();
+    for (let i = 0; i < data.length; i++) {
+      imgData.append('images', data[i]);
+    }
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    formData = new FormData();
+    const miniData = new FormData(e.currentTarget);
+    for (let [key, value] of miniData.entries()) {
+      if (key !== 'images') formData.append(key, value);
+    }
+    for (let [key, value] of imgData.entries()) {
+      formData.append(key, value);
+    }
+    for (const x of formData.entries()) {
+      console.log(x);
+    }
+  };
+
   return (
     <>
-    <section className='w-full h-[145px] lg:h-[200px] bg-[#0D0D0D] flex justify-center'>
-      <div className="from-main to-text bg-gradient-to-r bg-clip-text text-[24px] font-semibold text-transparent md:text-[30px] lg:text-[32px] flex items-center">
+      <section className="flex h-[145px] w-full justify-center bg-[#0D0D0D] lg:h-[200px]">
+        <div className="from-main to-text flex items-center bg-gradient-to-r bg-clip-text text-[24px] font-semibold text-transparent md:text-[30px] lg:text-[32px]">
           문의글 작성
         </div>
-    </section>
-    <section className="mx-auto mb-[20px] flex w-full max-w-[1220px] flex-col px-6 gap-[20px] lg:gap-[45px] mt-[40px] lg:mt-[50px] lg:mb-[80px]">
+      </section>
+      <form
+        onSubmit={handleSubmit}
+        className="mx-auto mt-[40px] mb-[20px] flex w-full max-w-[1220px] flex-col gap-[20px] px-6 lg:mt-[50px] lg:mb-[80px] lg:gap-[45px]"
+      >
         <WritingForm
+          name="title"
           title="제목"
           placeholder="제목을 입력해 주세요."
           isRequired
           isLongForm={false}
         />
 
-        <CategorySelect isRequired options={['신고', '버그', '기타 문의']} />
-
-        <WritingForm
-                  title="문의 내용"
-                  placeholder="문의에 관한 내용을 자세히 적어주세요."
-                  isRequired
-                  isLongForm
+        <CategorySelect
+          name="category"
+          isRequired
+          options={[
+            { key: 'GENERAL', value: '일반' },
+            { key: 'REPORT', value: '보고' }, // 내일 이름 물어보기
+          ]}
         />
 
-        <AddPhotoButton/>
+        <WritingForm
+          name="content"
+          title="문의 내용"
+          placeholder="문의에 관한 내용을 자세히 적어주세요."
+          isRequired
+          isLongForm
+        />
 
-        <div className='h-[1px] w-full bg-[#343434]'></div>
+        <AddPhotoButton onDataChange={handleDataChange} />
+
+        <div className="h-[1px] w-full bg-[#343434]"></div>
 
         <div>
-          <h3 className='text-[#fff] font-semibold text-[16px]'>안내사항</h3>
+          <h3 className="text-[16px] font-semibold text-[#fff]">안내사항</h3>
           <ul className="list-disc pl-5 text-[#959595]">
             <li>고객센터 운영시간은 10:00 ~ 19:00 예요.</li>
             <li>답변에는 시간이 소요됩니다. 조금만 기다려주세요.</li>
             <li>문의 내용을 자세하게 남겨주시면 빠른 답변에 도움이 됩니다.</li>
-            <li>문의하기 버튼을 누르시면 개인정보 수집에 동의하신 것으로 간주합니다.</li>
+            <li>
+              문의하기 버튼을 누르시면 개인정보 수집에 동의하신 것으로
+              간주합니다.
+            </li>
           </ul>
         </div>
 
-
-        <GrayButton className="mt-[20px] lg:mt-[30px] h-[52px] lg:h-[80px] text-[20px] font-semibold lg:text-[36px] bg-[#1F1F1F] text-[#5E5E5E]">작성하기</GrayButton>
-    </section>
+        <GrayButton className="mt-[20px] h-[52px] bg-[#1F1F1F] text-[20px] font-semibold text-[#5E5E5E] lg:mt-[30px] lg:h-[80px] lg:text-[36px]">
+          작성하기
+        </GrayButton>
+      </form>
     </>
   );
 }
