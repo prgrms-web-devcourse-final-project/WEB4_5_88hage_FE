ce762b4d4ff558bd171e626a67a403414ffda112
WON <won431236@gmail.com>
2025-07-24 16:48:30 +0900

    test

--- src/app/test/datepicker/page.tsx
+++ src/app/test/datepicker/page.tsx
@@ -1,11 +1,19 @@
+'use client';
 import DatepickerComponent from '@/components/common/DatepickerComponent';
+import { useEffect, useState } from 'react';
 
 export default function page() {
+  const [test, setTest] = useState('');
+
+  useEffect(() => {
+    console.log(test);
+  }, [test]);
+
   return (
     <div className="px-4 py-2">
       <DatepickerComponent
+        sendDate={(date) => setTest(date.toISOString())}
         placeholder="테스트입니당"
-        className="bg-amber-200"
       />
     </div>
   );
