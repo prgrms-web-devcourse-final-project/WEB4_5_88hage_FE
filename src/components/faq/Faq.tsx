712888f74bce8a9a2afbae3ba674e9af0039d720
Ujin <tladbwls0430@gmail.com>
2025-07-28 15:17:46 +0900

    faq공지사항 하나로

--- src/components/faq/Faq.tsx
+++ src/components/faq/Faq.tsx
@@ -2,7 +2,7 @@ import FaqAccordion from "./FaqAccordion";
 
 export default function FaqContent() {
   return (
-    <section className="bg-[#121212] text-white px-6 lg:px-24 pt-12 lg:pt-20 pb-32">
+    <section className="bg-[#151515] text-white px-6 lg:px-24 pt-12 lg:pt-20 pb-32">
       <div className="max-w-7xl mx-auto flex flex-col lg:flex-row gap-x-20">
         <div className="lg:w-1/3">
           <p className="hidden lg:block text-[16px] font-semibold text-[#06CE9E] mb-2">
