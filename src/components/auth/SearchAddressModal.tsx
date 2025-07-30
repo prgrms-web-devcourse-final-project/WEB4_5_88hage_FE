0492abc6db967c0e7e875a0b4c938bca4665ca9f
JUNHORANG <sjsk3358@naver.com>
2025-07-31 03:05:33 +0900

    도로명 찾기 모달 css 커스텀

--- src/components/auth/SearchAddressModal.tsx
+++ src/components/auth/SearchAddressModal.tsx
@@ -4,6 +4,7 @@
 import { searchAddress } from '@/lib/utils/getAddressXYGeocode';
 import React, { Dispatch, SetStateAction } from 'react';
 import DaumPostcode from 'react-daum-postcode';
+import '../../assets/styles/daum/daum.css'
 
 type setFc = {
   setShowModal: Dispatch<SetStateAction<boolean>>;
@@ -18,6 +19,18 @@ export default function SearchAddressModal({
   setLatitude,
   setLongitude,
 }: setFc) {
+
+  const themeObj = {
+   bgColor: "#313131", //바탕 배경색
+   searchBgColor: "#313131", //검색창 배경색
+   contentBgColor: "#545454", //본문 배경색(검색결과,결과없음,첫화면,검색서제스트)
+   pageBgColor: "#888888", //페이지 배경색
+   textColor: "#F2F2F2", //기본 글자색
+   queryTextColor: "#D0D0D0", //검색창 글자색
+   postcodeTextColor: "#1CEBB9", //우편번호 글자색
+   outlineColor: "#D0D0D0" //테두리
+};
+
   const closeModal = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
     setShowModal((prev) => !prev);
@@ -44,7 +57,7 @@ export default function SearchAddressModal({
         className="absolute top-0 right-0 z-200 flex h-screen w-full items-center justify-center bg-[rgba(0,0,0,0.6)] backdrop-blur-[20px]"
       >
         <div className="w-[400px] px-[20px]">
-          <DaumPostcode onComplete={onCompletePost} />
+          <DaumPostcode onComplete={onCompletePost} theme={themeObj} />
         </div>
       </div>
     </>
