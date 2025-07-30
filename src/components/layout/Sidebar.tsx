13cbac4f4daac10878836d284cc869e6f5bfdbd5
dlakrp123 <dlakrp731@gmail.com>
2025-07-31 02:23:35 +0900

    Feat: dashboard 라우팅 반응

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -7,8 +7,8 @@ import {
   List,
   Users,
 } from 'lucide-react';
-import { useState } from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const NAV_ITEMS = [
   { icon: CircleUserRound, label: '내 프로필', value: '/user/profile' },
@@ -19,7 +19,7 @@ const NAV_ITEMS = [
 ];
 
 export default function Sidebar() {
-  const [selectedIndex, setSelectedIndex] = useState(0);
+  const pathname = usePathname();
 
   return (
     <>
@@ -27,18 +27,15 @@ export default function Sidebar() {
       <div className="bg-gray-7 fixed bottom-0 left-0 z-50 flex h-[70px] w-full items-center justify-around lg:hidden">
         {NAV_ITEMS.map(({ icon: Icon, value }, idx) => (
           <Link href={value} key={idx}>
-            {' '}
-            {/* Link 컴포넌트로 감싸고 href 추가 */}
-            <button
-              onClick={() => setSelectedIndex(idx)}
+            <div
               className={`flex h-[40px] w-[40px] items-center justify-center rounded-full transition ${
-                selectedIndex === idx
+                pathname === value
                   ? 'bg-main text-black'
                   : 'bg-[#2c2c2c] text-white'
               }`}
             >
               <Icon size={22} />
-            </button>
+            </div>
           </Link>
         ))}
       </div>
@@ -47,27 +44,20 @@ export default function Sidebar() {
       <div className="bg-gray-7 hidden w-[220px] lg:fixed lg:top-0 lg:left-0 lg:flex lg:h-screen lg:flex-col lg:justify-start">
         <div className="py-30">
           <nav className="flex flex-col">
-            {NAV_ITEMS.map(
-              (
-                { icon: Icon, label, value },
-                idx, // value 추가
-              ) => (
-                <Link href={value} key={idx}>
-                  {/* Link 컴포넌트로 감싸고 href 추가 */}
-                  <button
-                    onClick={() => setSelectedIndex(idx)}
-                    className={`flex w-full items-center gap-3 rounded px-7 py-4 text-[18px] transition ${
-                      selectedIndex === idx
-                        ? 'bg-main text-black'
-                        : 'hover:bg-gray-8 text-white'
-                    }`}
-                  >
-                    <Icon size={22} />
-                    {label}
-                  </button>
-                </Link>
-              ),
-            )}
+            {NAV_ITEMS.map(({ icon: Icon, label, value }, idx) => (
+              <Link href={value} key={idx}>
+                <div
+                  className={`flex w-full items-center gap-3 rounded px-7 py-4 text-[18px] transition ${
+                    pathname === value
+                      ? 'bg-main text-black'
+                      : 'hover:bg-gray-8 text-white'
+                  }`}
+                >
+                  <Icon size={22} />
+                  {label}
+                </div>
+              </Link>
+            ))}
           </nav>
         </div>
       </div>
